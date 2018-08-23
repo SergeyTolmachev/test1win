c00fe31fbacb17ac728478513fa0b2e43f4e0c21
SergeyTolmachev <stolmachov@gmail.com>
2018-08-23 23:08:00 +0300

    1123: changed server message

--- src/routes/router.js
+++ src/routes/router.js
@@ -9,11 +9,7 @@ router.get('/book/:bookId', bookController.getBook);
 
 router.post('/book', bookController.createBook);
 
-
-router.update('/book?:dookId', bookController.updateBook);
-
-
-// router.put('/book/:bookId', bookController.updateBook);
+router.put('/book/:bookId', bookController.updateBook);
 
 module.exports = router;
 
